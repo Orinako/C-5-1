13deb103234b36b09d106da9d78591dca65cbd3f
Anna Podchasova <pod9696@gmail.com>
2022-11-03 15:02:43 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Задание_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string sentence = Console.ReadLine();
+
+            string[] words = SplitSentence(sentence);
+            OutputWords(words);
+
+            Console.ReadKey();
+        }
+
+        static string[] SplitSentence (string text)
+        {
+            string[] words = text.Split(' ');        
+            return words;
+        }
+
+        static void OutputWords (string[] wordsArray)
+        {
+            int i = 0;
+            while (i++ < wordsArray.Length)
+            {
+                Console.WriteLine($"{wordsArray[i-1]}");
+            }
+
+        }
+    }
+}
